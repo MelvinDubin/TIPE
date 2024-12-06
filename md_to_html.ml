d46e5b03b0dff66ff9c1b4a8bc30c0de6d0f5954
MelvinDubin <melvindubin47@gmail.com>
2024-12-06 13:11:47 +0100

    fonction qui ouvre un fichier markdown, et crée un fichier html l'ayant transformé

---
+++ md_to_html.ml
@@ -0,0 +1,16 @@
+
+(*Lit le fichier fileneame et renvoit son contenu dans une chaîne de caractères*)
+let lit_fichier (filename: string): string =
+  let file = open_in filename in
+  let taille_tot = in_channel_length file in
+  let s = Bytes.create taille_tot in
+  really_input file s 0 taille_tot;
+  close_in file;
+  Bytes.unsafe_to_string s
+
+(*Crée le fichir filename dans lequel est écrit contenu*)
+let markdown_to_html (filename_md: string) (filename_html: string): unit =
+  let lexemes_traites = pretraitement_lexeme (texte_to_lexeme_list (lit_fichier filename_md)) in
+  let file_out = open_out filename_html in
+  ecrit_en_html file_out (cree_arbre lexemes_traites);
+  close_out file_out
